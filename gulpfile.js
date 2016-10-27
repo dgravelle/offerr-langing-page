f14b92d99011e63129e50be8f83d7e2c7d777fe7
Dave Gravelle <dgravelle54@gmail.com>
2016-10-27 00:03:07 -0700

    fixed banner spacing

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
 gulp.task('watch', () => {
     gulp.watch('src/scss/**/*.scss', ['css'])
+    gulp.watch('src/js/**/*.ks', ['js'])
 })
 
 gulp.task('css', () => {
@@ -14,4 +16,11 @@ gulp.task('css', () => {
     .pipe(sass())
     .pipe(concat('home.css'))
     .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', () => {
+    return gulp.src('src/js/*.js')
+    .pipe(concat('bundle.js'))
+        .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(gulp.dest('dist/js'));
 });
